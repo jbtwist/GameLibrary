5c81b895bfab8cd059921d892d31195555fab9cf
Juan Bautista Villarejo Ortiz <jbtwist92@gmail.com>
2024-06-06 16:45:40 +0100

    Add unit testing and resolver libraries

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            'assets': './assets',
+            'components': './components',
+            'constants': '.constants',
+            'hooks': './hooks',
+            'scripts': './scripts',
+            'src': './src',
+            'tests': './tests'
+          }
+        }
+      ]
+    ]
   };
 };
